2853e49f06eda95a90d807da1f8ab1cdf14cc8ff
Krystian S <mr.krystian.stawarczyk@gmail.com>
2023-04-09 17:14:48 +0200

    updated npm builds

--- sanity_kuchnie/sanity.config.js
+++ sanity_kuchnie/sanity.config.js
@@ -6,10 +6,8 @@ import {schemaTypes} from './schemas'
 export default defineConfig({
   name: 'default',
   title: 'kuchnie',
-
-  projectId: '3go1z0b0',
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
   dataset: 'production',
-
   plugins: [deskTool(), visionTool()],
 
   schema: {
