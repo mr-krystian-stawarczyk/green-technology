78890ab6c58598a649fa976b1545bcbd8ef9fc42
Krystian S <mr.krystian.stawarczyk@gmail.com>
2023-04-09 17:02:56 +0200

    build run

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import { Layout } from "../components";
 import { PageTransition } from "next-page-transitions";
 import { useRouter } from "next/router";
 import { CSSTransition } from "react-transition-group";
-
+import { SSRProvider } from "react-bootstrap";
 export default function App(props) {
 	const { Component, pageProps, router } = props;
 
@@ -14,21 +14,23 @@ export default function App(props) {
 	}, []);
 
 	return (
-		<Layout>
-			<PageTransition
-				timeout={300}
-				classNames="page-transition"
-				loadingComponent={<div>Loading...</div>}
-				loadingDelay={500}
-				loadingClassNames="loading"
-				loadingTimeout={{
-					enter: 400,
-					exit: 0,
-				}}
-				skipInitialTransition={false}
-			>
-				<Component {...pageProps} key={router.route} />
-			</PageTransition>
-		</Layout>
+		<SSRProvider>
+			<Layout>
+				<PageTransition
+					timeout={300}
+					classNames="page-transition"
+					loadingComponent={<div>Loading...</div>}
+					loadingDelay={500}
+					loadingClassNames="loading"
+					loadingTimeout={{
+						enter: 400,
+						exit: 0,
+					}}
+					skipInitialTransition={false}
+				>
+					<Component {...pageProps} key={router.route} />
+				</PageTransition>
+			</Layout>
+		</SSRProvider>
 	);
 }
